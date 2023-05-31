cd6f6e4e3c89dd74f328b6146ff9b846e9ac2ec2
LokeshKanakala007 <lokeshkumarkanakala007@gmail.com>
2023-05-31 14:56:56 +0530

    workspace is added

---
+++ Addition_Of_Two_Numbers/src/com/loky/additon/Addition.java
@@ -0,0 +1,12 @@
+package com.loky.additon;
+
+public class Addition {
+
+	public static void main(String[] args) {
+		int num1 = 5;
+		int num2 = 10;
+		int result = num1+num2;
+		System.out.println("Addition of Two numbers..."+  result);
+	}
+
+}
